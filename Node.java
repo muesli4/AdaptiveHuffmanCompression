f9fd87cfe1e6fb5b7a83f01388bc8b7abccc027e
Niklas Jugl <niklas.jugl@gmail.com>
2013-05-04 15:51:34 +0200

    added constructor for leaf and node

--- Node.java
+++ Node.java
@@ -1,5 +1,10 @@
 
 public class Node extends PrefixTree {
 
+	public Node(PrefixTree parent, int weight, int number) {
+		super(parent, weight, number);
+		// TODO Auto-generated constructor stub
+	}
+
 }
 
