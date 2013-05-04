d203c02e84ebaa93014cfafe998f1186228ababa
muesli4 <muesli4@gmail.com>
2013-05-04 13:05:26 +0200

    basic functionality

--- PrefixTree.java
+++ PrefixTree.java
@@ -1,5 +1,42 @@
 
 public abstract class PrefixTree {
 
+    // parent of this tree
+    PrefixTree parent;
+
+    // weight of this tree (w)
+    int weight;
+    
+    // number of this tree (l)
+    int number;
+    
+    /**
+        Constructor with parent.
+    */
+    public PrefixTree(PrefixTree parent, int weight, int number) {
+    
+        this.parent = parent;
+        this.weight = weight;
+        this.number = number;
+    }
+
+    /**
+        Constructor without parent.
+    */
+    public PrefixTree(int weight, int number) {
+    
+        this.parent = null;
+        this.weight = weight;
+        this.number = number;
+    }
+
+    /**
+        Check whether this tee has a parent.
+    */
+    public boolean isRoot() {
+    
+        return parent == null;
+    }
+    
 }
 
