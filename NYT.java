5eb80b98c328ad14880de4ae7288f32312411721
muesli4 <muesli4@gmail.com>
2013-05-04 15:18:49 +0200

    NYT prefix tree typ added.

---
+++ NYT.java
@@ -0,0 +1,14 @@
+
+public class NYT extends PrefixTree {
+
+    public NYT(PrefixTree parent, int weight, int number) {
+    
+        super(parent, weight, number);
+    }
+
+    public NYT(int weight, int number) {
+    
+        super(weight, number);
+    }
+}
+
