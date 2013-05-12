01daa4dd6367a2abb2a53988aac40c787fe656b6
muesli4 <muesli4@gmail.com>
2013-05-12 17:26:43 +0200

    Added testing in Main.java (equality for input/output).

--- Main.java
+++ Main.java
@@ -1,62 +1,36 @@
-
+import java.io.BufferedInputStream;
 import java.io.PipedOutputStream;
 import java.io.PipedInputStream;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.io.PrintStream;
+
 
 public class Main {
 
-    public static void main(String[] args) {
-    
-        try {
-//            // create a manual pipe
-//            PipedInputStream is = new PipedInputStream();
-//            PipedOutputStream os = new PipedOutputStream(is);
-//        
-//            // connect both through the pipe
-//            PrintStream encoderStream = new PrintStream(new EncoderOutputStream(os));
-//            BufferedReader decoderReader = new BufferedReader(new InputStreamReader(new DecoderInputStream(is)));
-//
-//            // TODO test
-//            BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in));
-//            
-//            while (true) {
-//            
-//                String line = inputReader.readLine();
-//                
-//                if (line.equals("quit")) {
-//                    break;
-//                }
-//            
-//                encoderStream.println(line);
-//                System.out.println(decoderReader.readLine());
-//            }
+	public static void main(String[] args) {
 
-            PipedInputStream is = new PipedInputStream();
-            PipedOutputStream os = new PipedOutputStream();
-            is.connect(os);
-            
-            EncoderOutputStream eos = new EncoderOutputStream(os);
-            DecoderInputStream eis = new DecoderInputStream(is);
-            
-            
-            byte[] buff = new byte[]{97, 98, 99, 100};
-            eos.write(buff);
-            buff = new byte[4];
-            eis.read(buff);
-            
-            System.out.print((char)buff[0]);
-            System.out.print((char)buff[1]);
-            System.out.print((char)buff[2]);
-            System.out.print((char)buff[3]);
-            
-            
-        }
-        catch (Exception e) {
-        
-            System.err.println("error: " + e.getLocalizedMessage());
-        }
-    }
+		try {
+			PipedInputStream is = new PipedInputStream();
+			PipedOutputStream os = new PipedOutputStream();
+			is.connect(os);
+			
+			EncoderOutputStream eos = new EncoderOutputStream(os);
+			DecoderInputStream eis = new DecoderInputStream(is);
+
+			while (true) {
+			
+				int ch = System.in.read();
+				
+				if (ch == -1) {
+					
+					break;
+				}
+				
+				eos.write(ch);
+				System.out.print((char)eis.read());
+			}
+
+		} catch (Exception e) {
+
+			System.err.println("error: " + e.getLocalizedMessage());
+		}
+	}
 }
-
